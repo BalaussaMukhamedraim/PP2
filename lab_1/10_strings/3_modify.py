51fdc8f24b3f4c0182bfea81c1408a8e8b80899e
Balaussa <miss_balausa05@inbox.ru>
2025-01-20 00:59:18 +0500

    fifth commit

--- lab_1/10_strings/3_modify.py
+++ lab_1/10_strings/3_modify.py
@@ -3,3 +3,6 @@ print(a.upper())
 
 a = "Hello, World!"
 print(a.lower())
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
---
+++ lab_1/10_strings/7_stringMethods.py
@@ -0,0 +1,12 @@
+txt = "hello, and welcome to my world."
+
+x = txt.capitalize()
+
+print (x)
+
+
+txt = "Hello, welcome to my world."
+
+x = txt.find("welcome")
+
+print(x)
